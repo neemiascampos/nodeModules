27c12a58d6c0c215e7bb0d8e5043e1a389c620ca
neemiascampos <neemias.camposcruz@gmail.com>
2023-02-15 20:31:01 -0300

    update code v1.2

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ function App() {
   return (
       <main>
         <div>
-          <img src={qrCode} alt="" />
+          <img src={qrCode} alt="Imagem do QR Code" />
+          <br/><strong>Improve your front-end skills by building projects </strong>
+          <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
         </div>
       </main>
   )
